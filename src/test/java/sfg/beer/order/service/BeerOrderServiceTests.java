3bca5eaf002ad7d93cbaa9d93d73b012201bc08a
PierreSQS <nomo_messina@web.de>
2022-12-08 01:42:40 +0100

    upgraded SB to 2.7.6,Java from 11 to 17,MapStruct to 1.5.3 and Maven to 3.8.6

--- src/test/java/sfg/beer/order/service/BeerOrderServiceTests.java
+++ src/test/java/sfg/beer/order/service/BeerOrderServiceTests.java
@@ -1,16 +1,17 @@
 package sfg.beer.order.service;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.context.ApplicationContext;
 
-@RunWith(SpringRunner.class)
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
-public class BeerOrderServiceTests {
+class BeerOrderServiceTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads(ApplicationContext appCtx) {
+        assertThat(appCtx).isNotNull();
     }
 
 }
